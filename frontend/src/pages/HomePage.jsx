2e74317afc4f87886c15de2e02239623cd5ed5fb
Aman Jha <aman.jha21.aj@gmail.com>
2022-04-23 05:06:12 +0545

    added map and markers to home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import SimpleMap from "../components/SimpleMap";
+const AnyReactComponent = ({ text }) => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+    }}
+  >
+    <img
+      src="/location.png"
+      style={{
+        height: "40px",
+      }}
+    />
+    <div
+      style={{
+        color: "white",
+        paddingTop: "5px",
+        fontSize: "10px",
+        fontWeight: "bold",
+        width: "200px",
+      }}
+    >
+      {text}
+    </div>
+  </div>
+);
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <SimpleMap>
+      <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker 1" />
+      <AnyReactComponent lat={59.955413} lng={30.45885} text="My Marker 2" />
+    </SimpleMap>
+  );
 };
 
 export default HomePage;
