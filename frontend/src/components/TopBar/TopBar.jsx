3c3a850fa200021eeebd0595dcf901a5a7253e75
vaibhav kedia <vaibhavkedia24@gmail.com>
2022-04-23 03:14:49 +0530

    added imageLink parameter

--- frontend/src/components/TopBar/TopBar.jsx
+++ frontend/src/components/TopBar/TopBar.jsx
@@ -2,21 +2,20 @@ import Box from "@mui/material/Box";
 import AddIcon from "@mui/icons-material/Add";
 import "./TopBar.css";
 
-const TopBar = () => {
+const TopBar = ({
+  imageLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/FIDE_World_FR_Chess_Championship_2019_-_Magnus_Carlsen_%28cropped%29.jpg/640px-FIDE_World_FR_Chess_Championship_2019_-_Magnus_Carlsen_%28cropped%29.jpg",
+}) => {
   return (
     <Box className="container">
       <Box className="profileContainer">
         {/*Profile photo and name*/}
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/FIDE_World_FR_Chess_Championship_2019_-_Magnus_Carlsen_%28cropped%29.jpg/640px-FIDE_World_FR_Chess_Championship_2019_-_Magnus_Carlsen_%28cropped%29.jpg"
-          alt="no image"
-          className="profilePhoto"
-        />
+        <img src={imageLink} alt="no image" className="profilePhoto" />
         <Box className="username">Magnus Carlsen</Box>
       </Box>
       <Box className="logo">KALYAN</Box>
       <Box className="add">
         <AddIcon />
+        Create Event
       </Box>
     </Box>
   );
