f98b8e2723dd7e07d9fa60c2daee53d145c32844
BANGBANGROX <lakshya0809bang@gmail.com>
2022-04-23 00:27:21 +0530

    Updated SingleEventPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 // Components
 import TopBar from "./components/TopBar/TopBar";
 import CreateEventPage from "./pages/CreateEventPage/CreateEventPage";
+import SingleEventPage from "./pages/SingleEventPage/SingleEventPage";
 
 function App() {
   return (
     <>
       <TopBar />
-      <CreateEventPage />
+      <SingleEventPage />
     </>
   );
 }
