fb24d837c6da2e1efdf64ed02e490b8615a51d9a
vaibhav kedia <vaibhavkedia24@gmail.com>
2022-04-23 06:21:19 +0530

    Added cards in Landing page...

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -17,9 +17,66 @@ const LandingPage = () => {
             target amount and most importantly some verification document
             stating that their fundraiser is not a fraud for other users to see.
           </div>
+
           <div className="landing-card__ctr">
             <button className="landing-card__btn">Login</button>
           </div>
+
+          <section className="card-area">
+            <section className="card-section">
+              <div className="card">
+                <div className="card-front">
+                  <div className="card-front__tp card-front__tp--create">
+                    <h2 className="card-front__heading">
+                      Create Your Fundraiser
+                    </h2>
+                  </div>
+                  <div className="card-front__bt">
+                    <p className="card-front__text-view card-front__text-view--create">
+                      Kalyan helps you quickly create a fundraiser and tell your
+                      story.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <section className="card-section">
+              <div className="card">
+                <div className="card-front">
+                  <div className="card-front__tp card-front__tp--donate">
+                    <h2 className="card-front__heading">
+                      Donate to the fundraiser
+                    </h2>
+                  </div>
+                  <div className="card-front__bt">
+                    <p className="card-front__text-view card-front__text-view--donate">
+                      You can help with a donation of whatever amount you prefer
+                      for the social cause.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <section className="card-section">
+              <div className="card">
+                <div className="card-front">
+                  <div className="card-front__tp card-front__tp--accept">
+                    <h2 className="card-front__heading">
+                      Easily accept donations
+                    </h2>
+                  </div>
+                  <div className="card-front__bt">
+                    <p className="card-front__text-view card-front__text-view--accept">
+                      The funds raised can be directly transfer to your bank
+                      account without any hassle.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </section>
         </div>
       </div>
     </>
