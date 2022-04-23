ac986bc56795004e13170b2030446dc974b10468
vaibhav kedia <vaibhavkedia24@gmail.com>
2022-04-23 15:22:57 +0530

    minor

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -4,8 +4,8 @@ import "./Profile.css";
 
 const Profile = ({
   imageLink = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-  name = "Vinamra Singh",
-  email = "vinam115@gmail.com",
+  name = "Neha Singh",
+  email = "nehas115@gmail.com",
   points = "6",
 }) => {
   return (
