03e578d6565c5dd5d1375b210772380f5bf6a0c0
BANGBANGROX <lakshya0809bang@gmail.com>
2022-04-23 05:45:55 +0530

    minor css changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-
-// Components
-import TopBar from "../components/TopBar/TopBar";
-
-const HomePage = () => {
-  return (
-    <>
-      <TopBar />
-      <div>HomePage</div>
-    </>
-=======
+import { Hidden } from "@mui/material";
 import React, { useState } from "react";
 import SimpleMap from "../components/SimpleMap";
+import TopBar from "../components/TopBar/TopBar";
 const AnyReactComponent = ({ text }) => (
   <div
     style={{
@@ -56,19 +45,33 @@ const HomePage = () => {
   function showPosition(position) {
     console.log(position.coords.latitude, position.coords.longitude);
     setPosition({
-      lat: position.coords.latitude,
-      lng: position.coords.longitude,
+      // lat: position.coords.latitude - 7.639,
+      // lng: position.coords.longitude + 1.61,
+      lng: 86.14663,
+      lat: 22.775997,
     });
   }
 
   getLocation();
 
   return (
-    <SimpleMap center={position}>
-      <AnyReactComponent lat={25.5940947} lng={85.1375645} text="My Marker 1" />
-      <AnyReactComponent lat={25.6940947} lng={85.2375645} text="My Marker 2" />
-    </SimpleMap>
->>>>>>> 0b38ad02356b5c0758ca2ca026c25eb0b7f5bb9c
+    <>
+      <TopBar />
+      <div style={{ height: "90vh", overflow: "hidden" }}>
+        <SimpleMap center={position}>
+          <AnyReactComponent
+            lat={25.5940947}
+            lng={85.1375645}
+            text="My Marker 1"
+          />
+          <AnyReactComponent
+            lat={25.6940947}
+            lng={85.2375645}
+            text="My Marker 2"
+          />
+        </SimpleMap>
+      </div>
+    </>
   );
 };
 
